1c121e551eabf9c0ddfa7edf64ae0d55f414cfb6
Yurii Lenko <yura.lenko94@gmail.com>
2022-02-17 17:15:55 +0200

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {}
+  state = [] ||localStorage.getItem('state');
+  takeCryptData() {
+    localStorage.setItem("state", "111"); 
+    fetch('https://api.punkapi.com/v2/beers/')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("state", data); 
+        this.state = data;
+        console.log(data);
+      });
+  }
+
+  takeMenuData() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=Cocktail_glass')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("state", data.drinks[0]); 
+        this.state = data;
+        console.log(data.drinks[0]);
+      });
+  }
+}
